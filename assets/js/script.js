b9bdd027dec8547edd9d3e3b283069224830169d
Jubril Abdulsalam <abdulsalamjubr3@gmail.com>
2023-10-16 12:26:20 +0100

    completed project section layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 let mobileNav = document.querySelector('.mobile-nav');
 let mobileNavIcon = document.querySelector('.mobile-nav-icon');
 let primaryNav = document.querySelector('.primary-navigation');
+let projects = document.querySelectorAll('.project');
 
 mobileNav.addEventListener('click', () => {
     if (primaryNav.classList.contains('expanded')) {
@@ -15,3 +16,17 @@ mobileNav.addEventListener('click', () => {
         mobileNavIcon.classList.add('fa-xmark');
     }
 })
+
+/*for (let project of projects) {
+    project.addEventListener('mouseenter', () => {
+        let projectInfo = Array.from(document.querySelectorAll('.project-info'));
+        projectInfo.style.display = 'grid';
+    })
+}
+
+for (let project of projects) {
+    project.addEventListener('mouseleave', () => {
+        let projectInfo = document.querySelector('.project-info');
+        projectInfo.style.display = 'none';
+    })
+}*/
